3e24f65f74ec05ed76d43d8da08ca09963e3f303
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2017-01-20 20:37:25 +0100

    use bluebird library for promises

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-﻿module.exports = require("./lib/listenerCollection");
+module.exports = require('./lib/listenerCollection')
--- test/ListenerCollectionTest.js
+++ test/ListenerCollectionTest.js
@@ -1,268 +1,238 @@
-﻿/*globals describe, it, beforeEach, afterEach */
-
-var assert = require("assert"),
-    should = require("should"),
-    ListenerCollection = require("../"),
-    q = require("q");
+/* globals describe, it, beforeEach */
+var assert = require('assert')
+require('should')
+var ListenerCollection = require('../')
+var Promise = require('bluebird')
 
 describe('ListenersCollection', function () {
+  beforeEach(function () {
+    this.listeners = new ListenerCollection()
+  })
 
-    beforeEach(function () {
-        this.listeners = new ListenerCollection();
-    });
+  it('should fire listeners callback', function (done) {
+    var invokeCount = 0
 
-    it('should fire listeners callback', function (done) {
-        var invokeCount = 0;
+    this.listeners.add('test', function (next) {
+      invokeCount++
+      next()
+    })
 
-        this.listeners.add("test", function (next) {
-            invokeCount++;
-            next();
-        });
+    this.listeners.fire(function () {
+      assert.equal(1, invokeCount)
+      done()
+    })
+  })
 
-        this.listeners.fire(function () {
-            assert.equal(1, invokeCount);
-            done();
-        });
-    });
+  it('remove listener should remove function', function (done) {
+    var invokeCount = 0
 
-    it('remove listener should remove function', function (done) {
-        var invokeCount = 0;
+    this.listeners.add('test', function (next) {
+      invokeCount++
+      next()
+    })
 
-        this.listeners.add("test", function (next) {
-            invokeCount++;
-            next();
-        });
+    this.listeners.remove('test')
 
-        this.listeners.remove("test");
+    this.listeners.fire(function () {
+      assert.equal(0, invokeCount)
+      done()
+    })
+  })
 
-        this.listeners.fire(function () {
-            assert.equal(0, invokeCount);
-            done();
-        });
-    });
+  it('should fire listeners with arguments', function (done) {
+    this.listeners.add('test', function (a, b, next) {
+      assert.equal('param1', a)
+      assert.equal('param2', b)
+      next()
+    })
 
+    this.listeners.fire('param1', 'param2', function () {
+      done()
+    })
+  })
 
-    it('should fire listeners with arguments', function (done) {
+  it('should be able to use context', function (done) {
+    var context = {x: 1}
+    this.listeners.add('test', context, function (next) {
+      assert.equal(1, this.x)
+      next()
+    })
 
-        this.listeners.add("test", function (a, b, next) {
+    this.listeners.fire(function () {
+      done()
+    })
+  })
 
-            assert.equal("param1", a);
-            assert.equal("param2", b);
-            next();
-        });
+  it('insert should respect the after condition', function (done) {
+    var invocations = []
 
-        this.listeners.fire("param1", "param2", function () {
-            done();
-        });
-    });
+    this.listeners.add('test', function (next) {
+      invocations.push('test')
+      next()
+    })
 
-    it('should be able to use context', function (done) {
-        var context = {x: 1};
-        this.listeners.add("test", context, function (next) {
-            assert.equal(1, this.x);
-            next();
-        });
+    this.listeners.add('test3', function (next) {
+      invocations.push('test3')
+      next()
+    })
 
-        this.listeners.fire(function () {
-            done();
-        });
-    });
+    this.listeners.insert({ after: 'test' }, 'test2', this, function (next) {
+      invocations.push('test2')
+      next()
+    })
 
-    it('insert should respect the after condition', function (done) {
-        var invocations = [];
+    this.listeners.fire(function () {
+      invocations.should.have.length(3)
+      invocations[0].should.be.eql('test')
+      invocations[1].should.be.eql('test2')
+      invocations[2].should.be.eql('test3')
+      done()
+    })
+  })
 
-        this.listeners.add("test", function (next) {
-            invocations.push("test");
-            next();
-        });
+  it('insert should respect the after and before condition', function (done) {
+    var invocations = []
 
-        this.listeners.add("test3", function (next) {
-            invocations.push("test3");
-            next();
-        });
+    this.listeners.add('test3', function (next) {
+      invocations.push('test3')
+      next()
+    })
 
-        this.listeners.insert({ after: "test"}, "test2", this, function (next) {
-            invocations.push("test2");
-            next();
-        });
+    this.listeners.insert({ after: 'test', 'before': 'test3' }, 'test2', this, function (next) {
+      invocations.push('test2')
+      next()
+    })
 
+    this.listeners.fire(function () {
+      invocations.should.have.length(2)
+      invocations[0].should.be.eql('test2')
+      invocations[1].should.be.eql('test3')
+      done()
+    })
+  })
 
-        this.listeners.fire(function () {
-            invocations.should.have.length(3);
-            invocations[0].should.be.eql("test");
-            invocations[1].should.be.eql("test2");
-            invocations[2].should.be.eql("test3");
-            done();
-        });
-    });
+  it('insert should work if the conditioned element not present', function (done) {
+    var invocations = []
 
-    it('insert should respect the after and before condition', function (done) {
-        var invocations = [];
+    this.listeners.add('test3', function (next) {
+      invocations.push('test3')
+      next()
+    })
 
+    this.listeners.insert('test2', this, { after: 'test' }, function (next) {
+      invocations.push('test2')
+      next()
+    })
 
-        this.listeners.add("test3", function (next) {
-            invocations.push("test3");
-            next();
-        });
+    this.listeners.fire(function () {
+      invocations.should.have.length(2)
+      invocations[0].should.be.eql('test3')
+      invocations[1].should.be.eql('test2')
+      done()
+    })
+  })
 
-        this.listeners.insert({ after: "test", "before": "test3" }, "test2", this, function (next) {
-            invocations.push("test2");
-            next();
-        });
+  it('firePromise should return a valid awaitable promise', function () {
+    this.listeners.add('test', function () {
+      return Promise.resolve(1)
+    })
+    this.listeners.add('test', function () {
+      return Promise.resolve(2)
+    })
 
+    return this.listeners.fire().then(function (res) {
+      assert.equal(1, res[0])
+      assert.equal(2, res[1])
+    })
+  })
 
-        this.listeners.fire(function () {
-            invocations.should.have.length(2);
-            invocations[0].should.be.eql("test2");
-            invocations[1].should.be.eql("test3");
-            done();
-        });
-    });
+  it('firePromise should fire with arguments', function () {
+    var obj = {}
 
-    it('insert should work if the conditioned element not present', function (done) {
-        var invocations = [];
+    this.listeners.add('test', function (o) {
+      o.a = true
+    })
 
-        this.listeners.add("test3", function (next) {
-            invocations.push("test3");
-            next();
-        });
+    return this.listeners.fire(obj).then(function () {
+      assert.equal(true, obj.a)
+    })
+  })
 
-        this.listeners.insert("test2", this, { after: "test"}, function (next) {
-            invocations.push("test2");
-            next();
-        });
+  it('firePromise should return a valid promise that can catch errors', function () {
+    this.listeners.add('test', function () {
+      return Promise.reject(new Error('foo'))
+    })
 
+    return this.listeners.fire().catch(function () {
+    })
+  })
 
-        this.listeners.fire(function () {
-            invocations.should.have.length(2);
-            invocations[0].should.be.eql("test3");
-            invocations[1].should.be.eql("test2");
-            done();
-        });
-    });
+  it('firePromise rethrow error', function () {
+    var listeners2 = new ListenerCollection()
+    listeners2.add('test', function () {
+      return new Promise(function (resolve, reject) {
+        reject(new Error('foo'))
+      })
+    })
 
+    this.listeners.add('test', function () {
+      return new Promise(function (resolve, reject) {
+        resolve('ok')
+      })
+    })
 
-    it('firePromise should return a valid awaitable promise', function (done) {
+    return this.listeners.fire().then(function () {
+      return listeners2.fire()
+    }).catch(function (e) {
+      throw e
+    }).catch(function (e) {
+    })
+  })
 
-        this.listeners.add("test", function () {
-            return q(1);
-        });
-        this.listeners.add("test", function () {
-            return q(2);
-        });
+  it('firePromise should apply pre hooks', function () {
+    var i = 0
+    this.listeners.pre(function () {
+      i++
+    })
+    this.listeners.pre(function () {
+      i++
+    })
+    this.listeners.add('test', function () {
+      return Promise.resolve(1)
+    })
 
-        this.listeners.fire().then(function (res) {
-            assert.equal(1, res[0]);
-            assert.equal(2, res[1]);
-            done();
-        });
-    });
+    return this.listeners.fire().then(function (res) {
+      assert.equal(2, i)
+    })
+  })
 
-    it('firePromise should fire with arguments', function (done) {
-        var obj = {};
+  it('firePromise should apply post hooks', function () {
+    var postResult
+    this.listeners.post(function () {
+      postResult = this.key
+    })
 
-        this.listeners.add("test", function (o) {
-            o.a = true;
-        });
+    this.listeners.add('test', function () {
+      return Promise.resolve(1)
+    })
 
-        this.listeners.fire(obj).then(function () {
-            assert.equal(true, obj.a);
-            done();
-        });
-    });
+    return this.listeners.fire().then(function (res) {
+      assert.equal('test', postResult)
+    })
+  })
 
-    it('firePromise should return a valid promise that can catch errors', function (done) {
-        this.listeners.add("test", function () {
-            return q.reject(new Error("foo"));
-        });
+  it('firePromise should apply postError hooks', function () {
+    var error
+    this.listeners.postFail(function (err) {
+      error = err
+    })
 
-        this.listeners.fire().fail(function () {
-            done();
-        });
-    });
+    this.listeners.add('test', function () {
+      return Promise.reject(new Error('foo'))
+    })
 
-    it('firePromise rethrow error', function (done) {
-        var listeners2 = new ListenerCollection();
-        listeners2.add("test", function () {
-            return q().then(function() {
-                var defer = q.defer();
-                setTimeout(function () {
-                    defer.reject(new Error("foo"));
-                }, 0);
-
-                return defer.promise;
-            });
-        });
-
-        this.listeners.add("test", function () {
-            return q().then(function() {
-                var defer = q.defer();
-                setTimeout(function() {
-                    defer.resolve("ok");
-                },0);
-
-                return defer.promise;
-            });
-        });
-
-        this.listeners.fire().then(function() {
-            return listeners2.fire();
-        }).fail(function (e) {
-            throw e;
-        }).fail(function(e) {
-            done();
-        });
-    });
-
-    it('firePromise should apply pre hooks', function (done) {
-        var i = 0;
-        this.listeners.pre(function () {
-            i++;
-        });
-        this.listeners.pre(function () {
-            i++;
-        });
-        this.listeners.add("test", function () {
-            return q(1);
-        });
-
-        this.listeners.fire().then(function (res) {
-            assert.equal(2, i);
-            done();
-        });
-    });
-
-    it('firePromise should apply post hooks', function (done) {
-        var postResult;
-        this.listeners.post(function () {
-            postResult = this.key;
-        });
-
-        this.listeners.add("test", function () {
-            return q(1);
-        });
-
-        this.listeners.fire().then(function (res) {
-            assert.equal("test", postResult);
-            done();
-        });
-    });
-
-    it('firePromise should apply postError hooks', function (done) {
-        var error;
-        this.listeners.postFail(function (err) {
-            error = err;
-        });
-
-        this.listeners.add("test", function () {
-            return q.reject(new Error("foo"));
-        });
-
-        this.listeners.fire().fail(function (err) {
-            assert.equal(err, error);
-            done();
-        });
-    });
-
-
-});
+    return this.listeners.fire().catch(function (err) {
+      assert.equal(err, error)
+    })
+  })
+})
